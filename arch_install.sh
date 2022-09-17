fa6cad52499fd65575add9a7922a8786fad9f2a7
unknown <kagehayashi@gmail.com>
2022-09-17 11:44:41 -0700

    bruh

--- arch_install.sh
+++ arch_install.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
 echo "[*] Starting Arch Linux Speed Run..."
+
+echo "[*] Verifying boot mode..."
+ls /sys/firmware/efi/efivars 2> /dev/null
+
+if [[ $? -ne 0 ]]
+then 
+    echo "[-] UEFI not enabled"
+fi
+
+echo "[*] Exiting..."
